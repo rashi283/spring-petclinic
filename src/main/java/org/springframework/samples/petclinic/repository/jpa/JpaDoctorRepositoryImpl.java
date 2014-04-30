84f43abe653db9842d71bbea2523a29006b32f68
GeethaAnne <geethaanne.sjsu@gmail.com>
2014-04-29 19:27:57 -0700

    DoctorRepositoryjpa

---
+++ src/main/java/org/springframework/samples/petclinic/repository/jpa/JpaDoctorRepositoryImpl.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2002-2013 the original author or authors.
+ *
+package org.springframework.samples.petclinic.repository.jpa;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.samples.petclinic.model.Doctor;
+import org.springframework.samples.petclinic.model.DoctorType;
+import org.springframework.samples.petclinic.repository.DoctorRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class JpaDoctorRepositoryImpl implements DoctorRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<DoctorType> findDoctorTypes() {
+        return this.em.createQuery("SELECT ptype FROM DoctorType ptype ORDER BY ptype.name").getResultList();
+    }
+
+    @Override
+    public Doctor findById(int id) {
+        return this.em.find(Doctor.class, id);
+    }
+
+    @Override
+    public void save(Doctor doctor) {
+    	if (doctor.getId() == null) {
+    		this.em.persist(doctor);     		
+    	}
+    	else {
+    		this.em.merge(doctor);    
+    	}
+    }
+
+}
